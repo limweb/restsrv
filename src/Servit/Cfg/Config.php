0ec8c518065ead10539625345d088938be89abf1
limweb <limweb@hotmail.com>
2017-10-31 00:20:38 +0700

    .

---
+++ src/Servit/Cfg/Config.php
@@ -0,0 +1,24 @@
+<?php
+namespace  Servit\Cfg;
+
+class Config {
+
+    protected $values;
+    
+    public function __construct($obj=null){
+        if($obj) {
+        	$this->values = $obj;
+        } else {
+        	$this->values = new \stdClass();
+        }
+
+    }
+
+    public function __get($prop) {
+        return (isset($this->values->$prop) ? $this->values->$prop : null  );
+    }
+
+    public function __set( $prop, $value ) {
+             $this->values->$prop = $value;
+    }
+}
